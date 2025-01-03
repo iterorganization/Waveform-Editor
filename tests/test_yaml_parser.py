587ebea78eb9446f3e7763817b5bea356f155b32
Sebbe <sblokhuizen@ignitioncomputing.com>
2025-01-03 13:44:02 +0100

    extend pytest

--- tests/test_yaml_parser.py
+++ tests/test_yaml_parser.py
@@ -8,7 +8,32 @@ from waveform_editor.yaml_parser import YamlParser
 def test_yaml_parser():
     yaml_parser = YamlParser()
     yaml_parser.parse_waveforms("tests/test_yaml/test.yaml")
-    assert isinstance(yaml_parser.tendencies[0], LinearTendency)
-    assert isinstance(yaml_parser.tendencies[1], SineWaveTendency)
-    assert isinstance(yaml_parser.tendencies[2], ConstantTendency)
-    assert isinstance(yaml_parser.tendencies[3], SmoothTendency)
+    tendencies = yaml_parser.tendencies
+
+    assert isinstance(tendencies[0], LinearTendency)
+    assert tendencies[0].start == 0
+    assert tendencies[0].end == 5
+    assert tendencies[0].duration == 5
+    assert tendencies[0].from_value == 0
+    assert tendencies[0].to_value == 8
+
+    assert isinstance(tendencies[1], SineWaveTendency)
+    assert tendencies[1].start == 5
+    assert tendencies[1].end == 9
+    assert tendencies[1].duration == 4
+    assert tendencies[1].amplitude == 2
+    assert tendencies[1].frequency == 1
+    assert tendencies[1].base == 8
+
+    assert isinstance(tendencies[2], ConstantTendency)
+    assert tendencies[2].start == 9
+    assert tendencies[2].end == 12
+    assert tendencies[2].duration == 3
+    assert tendencies[2].value == 8
+
+    assert isinstance(tendencies[3], SmoothTendency)
+    assert tendencies[3].start == 12
+    assert tendencies[3].end == 14
+    assert tendencies[3].duration == 2
+    assert tendencies[3].from_value == 8
+    assert tendencies[3].to_value == 0
