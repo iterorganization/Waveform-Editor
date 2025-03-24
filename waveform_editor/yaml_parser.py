03a56caeedebef92f6cbb9e36c88dde89fc79726
Sebbe <sblokhuizen@ignitioncomputing.com>
2025-03-24 10:23:19 +0100

    allow for a single number to be used as shorthand for a constant tendency

--- waveform_editor/yaml_parser.py
+++ waveform_editor/yaml_parser.py
@@ -75,7 +75,8 @@ class YamlParser:
                 )
 
             waveform = waveform_yaml.get("user_waveform", [])
-            self.waveform = Waveform(waveform=waveform)
+            line_number = waveform_yaml.get("line_number", 0)
+            self.waveform = Waveform(waveform=waveform, line_number=line_number)
         except yaml.YAMLError as e:
             self._handle_yaml_error(e)
 
