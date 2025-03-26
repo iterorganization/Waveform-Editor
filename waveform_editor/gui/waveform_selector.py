57c507feb83918b801f83e725a5bc55dbafd3839
Sebbe <sblokhuizen@ignitioncomputing.com>
2025-03-26 14:49:25 +0100

    WIP: add waveform adder widget to all groups

--- waveform_editor/gui/waveform_selector.py
+++ waveform_editor/gui/waveform_selector.py
@@ -19,17 +19,20 @@ class WaveformSelector:
         self.selected_keys = []
         self.previous_selection = {}
         self.yaml_map = {}
-        self.selector = self.create_waveform_selector(yaml_data)
+        self.selector = self.create_waveform_selector(yaml_data, is_root=True)
         self.deselect_logic_enabled = False  # Control flag for deselect behavior
 
     def enable_deselect_logic(self, enable):
         """Enable or disable the deselect logic based on the tab selection."""
         self.deselect_logic_enabled = enable
 
-    def create_waveform_selector(self, data):
+    def create_waveform_selector(self, data, is_root=False):
         """Recursively create a Panel UI structure from the YAML."""
         categories = []
         options = []
+
+        if data is None:
+            return
 
         for key, value in data.items():
             if key == "globals":
@@ -41,101 +44,109 @@ class WaveformSelector:
                 categories.append((key, self.create_waveform_selector(value)))
 
         content = []
-        if options:
-            check_buttons = pn.widgets.CheckButtonGroup(
-                value=[],
-                options=options,
-                button_style="outline",
-                button_type="primary",
-                sizing_mode="stretch_width",
-                orientation="vertical",
-                stylesheets=["button {text-align: left!important;}"],
-            )
+        check_buttons = pn.widgets.CheckButtonGroup(
+            value=[],
+            options=options,
+            button_style="outline",
+            button_type="primary",
+            sizing_mode="stretch_width",
+            orientation="vertical",
+            stylesheets=["button {text-align: left!important;}"],
+        )
 
-            select_all_button = pn.widgets.ButtonIcon(
-                icon="select-all", size="30px", active_icon="check"
-            )
+        select_all_button = pn.widgets.ButtonIcon(
+            icon="select-all", size="30px", active_icon="check"
+        )
 
-            def select_all(event):
-                """Select all options in this CheckButtonGroup."""
-                check_buttons.value = options
+        def select_all(event):
+            """Select all options in this CheckButtonGroup."""
+            check_buttons.value = options
 
-            select_all_button.on_click(select_all)
+        select_all_button.on_click(select_all)
 
-            deselect_all_button = pn.widgets.ButtonIcon(
-                icon="deselect", size="30px", active_icon="check"
-            )
+        deselect_all_button = pn.widgets.ButtonIcon(
+            icon="deselect", size="30px", active_icon="check"
+        )
 
-            def deselect_all(event):
-                """Deselect all options in this CheckButtonGroup."""
-                check_buttons.value = []
+        def deselect_all(event):
+            """Deselect all options in this CheckButtonGroup."""
+            check_buttons.value = []
 
-            deselect_all_button.on_click(deselect_all)
+        deselect_all_button.on_click(deselect_all)
 
-            def on_select(event):
-                new_selection = event.new
-                old_selection = self.previous_selection.get(check_buttons, [])
-                newly_selected = [
-                    key for key in new_selection if key not in old_selection
-                ]
-                if self.deselect_logic_enabled:
-                    # Deselect all except for newly_selected
-                    if newly_selected:
-                        newly_selected = newly_selected[0]
-                        self.deselect_all(exclude=newly_selected)
+        def on_select(event):
+            new_selection = event.new
+            old_selection = self.previous_selection.get(check_buttons, [])
+            newly_selected = [key for key in new_selection if key not in old_selection]
+            if self.deselect_logic_enabled:
+                # Deselect all except for newly_selected
+                if newly_selected:
+                    newly_selected = newly_selected[0]
+                    self.deselect_all(exclude=newly_selected)
 
-                        value = self.yaml_map[newly_selected]
-                        if isinstance(value, (int, float)):
-                            yaml_dump = f"{newly_selected}: {value}"
-                        else:
-                            yaml_dump = f"{newly_selected}:\n{yaml.dump(value)}"
-                        self.waveform_editor.code_editor.value = yaml_dump
-                else:
-                    deselected = [
-                        key for key in old_selection if key not in new_selection
-                    ]
-                    self.selected_keys = list(
-                        set(self.selected_keys + newly_selected) - set(deselected)
-                    )
-                    self.waveform_plotter.selected_keys = self.selected_keys
-                self.previous_selection[check_buttons] = new_selection
+                    value = self.yaml_map[newly_selected]
+                    if isinstance(value, (int, float)):
+                        yaml_dump = f"{newly_selected}: {value}"
+                    else:
+                        yaml_dump = f"{newly_selected}:\n{yaml.dump(value)}"
+                    self.waveform_editor.code_editor.value = yaml_dump
+            else:
+                deselected = [key for key in old_selection if key not in new_selection]
+                self.selected_keys = list(
+                    set(self.selected_keys + newly_selected) - set(deselected)
+                )
+                self.waveform_plotter.selected_keys = self.selected_keys
+            self.previous_selection[check_buttons] = new_selection
 
-            check_buttons.param.watch(on_select, "value")
+        check_buttons.param.watch(on_select, "value")
 
-            plus_button = pn.widgets.ButtonIcon(
-                icon="plus", size="30px", active_icon="check"
-            )
+        # Widget for adding new waveforms
+        new_waveform_button = pn.widgets.ButtonIcon(
+            icon="plus", size="30px", active_icon="check"
+        )
+        new_waveform_input = pn.widgets.TextInput(
+            placeholder="Enter name of new waveform"
+        )
+        add_new_waveform_button = pn.widgets.Button(
+            name="Add", sizing_mode="stretch_width"
+        )
+        new_waveform_panel = pn.Row(new_waveform_input, add_new_waveform_button)
+        new_waveform_panel.visible = False
 
-            # Widget for adding new waveforms
-            new_option_input = pn.widgets.TextInput(
-                placeholder="Enter name of new waveform"
-            )
-            new_option_button = pn.widgets.Button(
-                name="Add", sizing_mode="stretch_width"
-            )
-            add_option_panel = pn.Row(new_option_input, new_option_button)
-            add_option_panel.visible = False
+        def on_add_waveform_button_click(event):
+            """Show the text input to add a new option."""
+            new_waveform_panel.visible = True
 
-            def on_plus_button_click(event):
-                """Show the text input to add a new option."""
-                add_option_panel.visible = True
+        def add_new_waveform(event):
+            """Add the new option to CheckButtonGroup."""
+            new_waveform = new_waveform_input.value.strip()
+            if new_waveform and new_waveform not in check_buttons.options:
+                check_buttons.options.append(new_waveform)
+                new_waveform_panel.visible = False
+                new_waveform_input.value = ""
+                check_buttons.param.trigger("options")
+                select_all_button.visible = True
+                deselect_all_button.visible = True
 
-            def add_new_option(event):
-                """Add the new option to CheckButtonGroup."""
-                new_option = new_option_input.value.strip()
-                if new_option and new_option not in check_buttons.options:
-                    check_buttons.options.append(new_option)
-                    add_option_panel.visible = False
-                    new_option_input.value = ""
-                    check_buttons.param.trigger("options")
+        new_waveform_button.on_click(on_add_waveform_button_click)
+        add_new_waveform_button.on_click(add_new_waveform)
 
-            plus_button.on_click(on_plus_button_click)
-            new_option_button.on_click(add_new_option)
+        # Add buttons
+        button_row = pn.Row(
+            new_waveform_button,
+            select_all_button,
+            deselect_all_button,
+        )
 
-            button_row = pn.Row(select_all_button, deselect_all_button, plus_button)
-            content.append(button_row)
-            content.append(add_option_panel)
-            content.append(check_buttons)
+        if not options:
+            select_all_button.visible = False
+            deselect_all_button.visible = False
+        if is_root:
+            new_waveform_button.visible = False
+
+        content.append(button_row)
+        content.append(new_waveform_panel)
+        content.append(check_buttons)
 
         if categories:
             accordion = pn.Accordion(*categories)
