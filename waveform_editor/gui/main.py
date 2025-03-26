3fbfd5e6db077e98c4666b81559084c1e2869c5c
Sebbe <sblokhuizen@ignitioncomputing.com>
2025-03-26 11:57:11 +0100

    add file input

--- waveform_editor/gui/main.py
+++ waveform_editor/gui/main.py
@@ -14,42 +14,62 @@ hv.extension("bokeh")
 
 
 class WaveformEditorGui:
-    def __init__(self, yaml_file):
+    def __init__(self):
         """Initialize the Waveform Editor Panel App"""
-        with open(yaml_file) as f:
-            yaml_data = yaml.safe_load(f)
+        self.file_input = pn.widgets.FileInput(accept=".yaml")
+        self.file_input.param.watch(self.load_yaml, "value")
 
-        editor = WaveformEditor()
-        waveform_plotter = WaveformPlotter(yaml_data)
-        waveform_selector = WaveformSelector(yaml_data, waveform_plotter, editor)
-
-        # Tabs to handle the "View" and "Edit" tabs
-        tabs = pn.Tabs(
-            ("View Waveforms", waveform_plotter.get_dynamic_map()),
-            ("Edit Waveforms", editor.get_layout()),
-            dynamic=True,
+        self.tabs = pn.Tabs(dynamic=True)
+        self.template = pn.template.FastListTemplate(
+            title=f"Waveform Editor (v{waveform_editor.__version__})",
+            main=self.tabs,
+            sidebar_width=400,
         )
+        # To update the sidebar we wrap it into a panel column object and update this.
+        # This trick is taken from:
+        # https://discourse.holoviz.org/t/callbacks-to-update-widgets-in-bootstrap-
+        # template-sidebar-based-on-active-tab/1535/2
+        self.sidebar_column = pn.Column(
+            pn.Column(
+                pn.pane.Markdown("## Select YAML File", margin=0),
+                self.file_input,
+            )
+        )
+        self.template.sidebar.append(self.sidebar_column)
 
+        # Change selection logic depending on which tab is selected
         def on_tab_change(event):
             # Check if "Edit Waveforms" tab is selected (index 1)
             if event.new == 1:
-                waveform_selector.deselect_all()
-                waveform_selector.enable_deselect_logic(True)
+                self.waveform_selector.deselect_all()
+                self.waveform_selector.enable_deselect_logic(True)
             else:
-                waveform_selector.enable_deselect_logic(False)
+                self.waveform_selector.enable_deselect_logic(False)
 
-        tabs.param.watch(on_tab_change, "active")
+        self.tabs.param.watch(on_tab_change, "active")
 
-        sidebar = pn.Column(
-            pn.pane.Markdown(f"## File: `{yaml_file}`", margin=0),
-            waveform_selector.get_selector(),
+    def load_yaml(self, event):
+        """Load YAML data from uploaded file"""
+        yaml_content = event.new.decode("utf-8")
+        self.yaml_data = yaml.safe_load(yaml_content)
+
+        editor = WaveformEditor()
+        self.waveform_plotter = WaveformPlotter(self.yaml_data)
+        self.waveform_selector = WaveformSelector(
+            self.yaml_data, self.waveform_plotter, editor
         )
 
-        self.template = pn.template.FastListTemplate(
-            title=f"Waveform Editor (v{waveform_editor.__version__})",
-            sidebar=sidebar,
-            main=tabs,
-            sidebar_width=400,
+        self.tabs[:] = [
+            ("View Waveforms", self.waveform_plotter.get_dynamic_map()),
+            ("Edit Waveforms", editor.get_layout()),
+        ]
+
+        self.sidebar_column[0] = pn.Column(
+            pn.pane.Markdown(
+                f"## Succesfully loaded `{self.file_input.filename}`", margin=0
+            ),
+            self.file_input,
+            self.waveform_selector.get_selector(),
         )
 
     def serve(self):
@@ -58,5 +78,4 @@ class WaveformEditorGui:
 
 
 # Run the app
-yaml_file = "waveform_editor/test.yaml"
-WaveformEditorGui(yaml_file).serve()
+WaveformEditorGui().serve()
