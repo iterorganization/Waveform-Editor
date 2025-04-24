72df491b493732f024dd496aaf3805a6b75ac54a
Sebbe <sblokhuizen@ignitioncomputing.com>
2025-04-24 14:41:51 +0200

    update docstrings

--- waveform_editor/exporter.py
+++ waveform_editor/exporter.py
@@ -81,18 +81,45 @@ class ConfigurationExporter:
             self._fill_node_recursively(ids, path, values)
 
     def _fill_node_recursively(self, ids_node, path, values):
+        """Recursively traverses the IDS node along the given path, and fills the
+        values at the leaf node.
+
+        Args:
+            ids_node: The current IDS node.
+            path: An IDSPath object.
+            values: Values to set the IDS quantity values to.
+        """
         if len(path.parts) == 0:
-            self._fill_values(ids_node, path, values)
+            self._fill_values(ids_node, values)
             return
 
         self._traverse_node(ids_node, path, values)
 
     def _traverse_path(self, path):
+        """Removes the first path part from an IDSPath.
+
+        For example:
+            IDSPath("beam(2)/phase/angle") --> IDSPath("phase/angle")
+
+        Args:
+            path: The IDSPath to remove the first part from.
+
+        Returns:
+            The IDSPath without the first path part.
+        """
         if len(path.parts) == 1:
             return IDSPath("")
         return IDSPath(str(path).split("/", 1)[1])
 
     def _traverse_node(self, ids_node, path, values):
+        """Traverses an IDS node according to the current part of the path and handles
+        indexing, slicing, and time coordinate expansion.
+
+        Parameters:
+            ids_node: The current IDS node.
+            path: An IDSPath object.
+            values: Values to set the IDS quantity values to.
+        """
         part = path.parts[0]
         index = path.indices[0]
         current = ids_node[part]
@@ -121,29 +148,58 @@ class ConfigurationExporter:
         else:
             self._fill_node_recursively(current, self._traverse_path(path), values)
 
-    def _fill_values(self, ids_node, path, values):
+    def _fill_values(self, ids_node, values):
+        """Sets values on a leaf node, ensuring data types match expectations.
+
+        Parameters:
+            ids_node: The IDS node to fill values into.
+            values: The value(s) to assign.
+        """
+        # TODO: This error handling should be done when the waveforms are made,
+        # instead of during the export.
         if not hasattr(ids_node, "data_type"):
-            raise ValueError(f"Waveform {path} is not a 'FLT_0D' or 'FLT_1D'.")
+            raise ValueError(
+                f"{ids_node.metadata.name!r} is not a 'FLT_0D' or 'FLT_1D'."
+            )
 
         if ids_node.data_type == "FLT_1D":
+            if not ids_node.metadata.coordinate1.is_time_coordinate:
+                raise ValueError(
+                    f"{ids_node.metadata.name!r} is not a time-dependent quantity"
+                )
             ids_node.value = values
         elif ids_node.data_type == "FLT_0D":
+            # If the values are not split, the path does not contain a time dependent
+            # node
             if not np.isscalar(values):
-                raise ValueError(f"Expected scalar value for {path}, got: {values}")
+                raise ValueError(
+                    f"{ids_node.metadata.name!r} is not a time-dependent quantity"
+                )
             ids_node.value = values
         else:
-            raise ValueError(f"{path} has unsupported data_type: {ids_node.data_type}.")
+            raise ValueError(
+                f"{ids_node.metadata.name!r} has unsupported data_type: "
+                f"{ids_node.data_type}."
+            )
 
-    def _traverse_slice(self, current, slice, path, values):
+    def _traverse_slice(self, ids_node, slice, path, values):
+        """Traverses sliced IDS nodes.
+
+        Parameters:
+            ids_node: The IDS node to traverse the slice of.
+            slice: The slice object defining the range of indices.
+            path: The IDSPath object.
+            values: Values to set the IDS quantity values to.
+        """
         if slice.start is None and slice.stop is None:
             start = 0
-            stop = len(current) or 1
+            stop = len(ids_node) or 1
         else:
             start = slice.start if slice.start is not None else 0
-            stop = slice.stop if slice.stop is not None else len(current) or start + 1
+            stop = slice.stop if slice.stop is not None else len(ids_node) or start + 1
         max_index = max(start, stop - 1)
-        if len(current) <= max_index:
-            current.resize(max_index + 1, keep=True)
+        if len(ids_node) <= max_index:
+            ids_node.resize(max_index + 1, keep=True)
 
         for i in range(start, stop):
-            self._fill_node_recursively(current[i], self._traverse_path(path), values)
+            self._fill_node_recursively(ids_node[i], self._traverse_path(path), values)
