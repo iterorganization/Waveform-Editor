ecf4a26b5d0104d072fd121da237a44df6da3373
Sebbe <sblokhuizen@ignitioncomputing.com>
2025-07-16 15:26:45 +0200

    hide bind object

--- waveform_editor/gui/io/manager.py
+++ waveform_editor/gui/io/manager.py
@@ -20,10 +20,10 @@ class IOManager(Viewer):
     menu_items = param.List()
 
     def __init__(self, main_gui, **params):
+        self.open_file_text = pn.widgets.StaticText(width=250, align="center")
+        self.menu = pn.widgets.MenuButton(name="File", width=120, margin=(15, 10))
         super().__init__(**params)
         self.main_gui = main_gui
-        self.open_file_text = pn.pane.Markdown(width=280, margin=0)
-        self.menu = pn.widgets.MenuButton(name="File", width=120, margin=(15, 10))
 
         self.file_loader = FileLoader(self)
         self.file_saver = FileSaver(self)
@@ -31,16 +31,17 @@ class IOManager(Viewer):
 
         self.panel = pn.Column(
             pn.Row(self.menu, self.open_file_text),
-            pn.bind(self._handle_menu_selection, self.menu.param.clicked),
+            pn.Row(  # Ensure the bind doesn't take any UI space
+                pn.bind(self._handle_menu_selection, self.menu.param.clicked),
+                visible=False,
+            ),
             self.file_loader,
             self.file_saver,
             self.file_exporter.modal,
             visible=self.param.visible,
         )
 
-        self._set_menu_items()
-
-    @param.depends("is_editing", watch=True)
+    @param.depends("is_editing", watch=True, on_init=True)
     def _set_menu_items(self):
         if self.is_editing:
             self.menu.items = [NEW, OPEN, SAVE, SAVE_AS, EXPORT]
@@ -66,12 +67,14 @@ class IOManager(Viewer):
         # Menu items seem to not retrigger when selecting same twice in a row
         self.menu.clicked = None
 
-    @param.depends("is_editing", "open_file", watch=True)
+    @param.depends("is_editing", "open_file", watch=True, on_init=True)
     def _set_open_file_text(self):
-        if self.open_file:
-            self.open_file_text.object = f"`{self.open_file}`"
+        if not self.is_editing:
+            self.open_file_text.value = "Create or open a YAML file"
+        elif self.open_file:
+            self.open_file_text.value = f"{self.open_file}"
         else:
-            self.open_file_text.object = "_No file is currently opened_"
+            self.open_file_text.value = "No file is currently opened"
 
     def __panel__(self):
         return self.panel
