c819fac76a064954accc3bdfc08870ac16662c4b
Maarten Sebregts <msebregts@ignitioncomputing.com>
2025-07-16 12:23:58 +0200

    Add improved file browser dialogs

---
+++ waveform_editor/gui/filedialog.py
@@ -0,0 +1,132 @@
+import os
+from typing import ClassVar
+
+import param
+from panel.layout import Column, Modal, Row
+from panel.viewable import Viewer
+from panel.widgets import Button, TextInput
+
+from waveform_editor.gui.filebrowser import FileBrowser
+
+
+class FileDialogBase(Viewer):
+    """Common logic for the Open and Save file dialogs."""
+
+    _cancel_text: ClassVar[str] = "Cancel"
+    _confirm_text: ClassVar[str]
+
+    multiselect = param.Boolean(default=True)
+
+    def __init__(self, root_directory, **params):
+        super().__init__(**params)
+        self.on_confirm = None
+        self.filebrowser = FileBrowser(
+            multiselect=self.param.multiselect,
+            root_directory=root_directory,
+        )
+        self.input = TextInput(name="Filename", sizing_mode="stretch_width")
+        self.confirm = Button(
+            name=self._confirm_text,
+            on_click=self._confirm,
+            disabled=self.input.param.value_input.rx.not_(),
+        )
+        self.cancel = Button(name=self._cancel_text, on_click=self.close)
+        self.modal = Modal(
+            self.filebrowser,
+            Row(self.input, Column(self.confirm, self.cancel)),
+            open=False,
+            show_close_button=False,
+            background_close=True,
+            width=800,
+        )
+
+        self.filebrowser.param.watch(self._on_fileselection_change, "value")
+        self.filebrowser.param.watch(self._confirm, "doubleclick")
+        self.input.param.watch(self._confirm, "enter_pressed")
+
+    def _on_fileselection_change(self, event):
+        if event.new:
+            self.input.value = ", ".join(os.path.basename(v) for v in event.new)
+
+    def open(self, directory, fname=None, on_confirm=None):
+        """Open the dialog.
+
+        The ``on_confirm`` callback will be called when the user double-clicks a file,
+        presses Enter in the text input, or clicks the "Open"/"Save" button. It will
+        receive a list of selected file names as only argument. If the callback returns
+        True, the dialog will be closed.
+
+        Args:
+            directory: Directory to show in the file dialog
+            fname: Preselected filename
+            on_confirm: Callback when the dialog is confirmed
+        """
+        self.on_confirm = on_confirm
+        self.filebrowser.directory = directory
+        self.input.value = fname or ""
+        self.modal.show()
+
+    def _get_input_fname(self):
+        return os.path.join(self.filebrowser.directory, self.input.value)
+
+    def _confirm(self, event=None):
+        if self.on_confirm:
+            if self.input.disabled:
+                fnames = self.filebrowser.value
+            else:
+                fnames = [self._get_input_fname()]
+            if self.on_confirm(fnames):
+                self.close()
+
+    def close(self, event=None):
+        """Close the dialog."""
+        self.modal.hide()
+
+    def __panel__(self):
+        return self.modal
+
+
+class SaveFileDialog(FileDialogBase):
+    """A dialog for saving files.
+
+    Example:
+        .. code-block:: python
+
+            def on_confirm(fnames):
+                if not fnames: return False
+                fname = fnames[0]
+                with open(fname, "w") as f:
+                    f.write(data)
+                return True
+
+            dialog = SaveFileDialog("/")
+            dialog.open("/path/to/folder", "filename.txt", on_confirm)
+    """
+
+    _confirm_text = "Save"
+
+    def __init__(self, root_directory):
+        super().__init__(root_directory=root_directory, multiselect=False)
+
+
+class OpenFileDialog(FileDialogBase):
+    """A dialog for opening files.
+
+    Example:
+        .. code-block:: python
+
+            def on_confirm(fnames):
+                if not fnames: return False
+                for fname in fnames:
+                    print("Opening file:", fname)
+                return True
+
+            dialog = OpenFileDialog("/")
+            dialog.open("/path/to/folder", "", on_confirm)
+    """
+
+    _confirm_text = "Open"
+
+    def __init__(self, root_directory):
+        super().__init__(root_directory=root_directory)
+        self.input.disabled = True
