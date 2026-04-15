337117c9dea4f27dc964b9f9ba679d5e6021a5d4
Alexandra Ioan <ioan99alexandra@gmail.com>
2026-04-15 17:28:42 +0200

    merge back the nice integrations file

--- waveform_editor/gui/shape_editor/shape_editor.py
+++ waveform_editor/gui/shape_editor/shape_editor.py
@@ -10,11 +10,11 @@ from panel.viewable import Viewer
 
 from waveform_editor.gui.settings import nice_settings_panel
 from waveform_editor.gui.shape_editor.coil_currents import CoilCurrents
-from waveform_editor.gui.shape_editor.nice_integration import NiceIntegration
 from waveform_editor.gui.shape_editor.nice_plotter import NicePlotter
 from waveform_editor.gui.shape_editor.plasma_properties import PlasmaProperties
 from waveform_editor.gui.shape_editor.plasma_shape import PlasmaShape
 from waveform_editor.settings import NiceSettings, settings
+from waveform_editor.shape_editor.nice_integration import NiceIntegration
 
 logger = logging.getLogger(__name__)
 
@@ -36,7 +36,13 @@ class ShapeEditor(Viewer):
     def __init__(self, main_gui):
         super().__init__()
         self.factory = imas.IDSFactory()
-        self.communicator = NiceIntegration(self.factory)
+        self.terminal = pn.widgets.Terminal(
+            sizing_mode="stretch_width",
+            options={"scrollback": 10000, "wrap": True},
+            height=200,
+            max_width=750,
+        )
+        self.communicator = NiceIntegration(self.factory, on_output=self.terminal.write)
         self.plasma_shape = PlasmaShape()
         self.plasma_properties = PlasmaProperties()
         self.coil_currents = CoilCurrents(main_gui)
@@ -96,7 +102,7 @@ class ShapeEditor(Viewer):
             self._create_card(self.coil_currents, "Coil Currents"),
         )
         menu = pn.Column(
-            buttons, self.communicator.terminal, sizing_mode="stretch_width"
+            buttons, self.terminal, sizing_mode="stretch_width"
         )
         self.panel = pn.Row(
             self.nice_plotter.flux_map_pane,
